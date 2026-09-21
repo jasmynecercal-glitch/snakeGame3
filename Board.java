c5163a3fa6d349a38419bb61383928f17055d5e5
jasmynecercal-glitch <jasmyne.cercal@escola.pr.gov.br>
2026-09-21 11:51:36 +0000

    versao3

---
+++ Board.java
@@ -0,0 +1,15 @@
+public class Board {
+public void draw(Graphics2D g2) {
+}
+private void drawGrass (Graphics2D g2) {
+I
+int tile GameConstants.TILE;
+for(int x = 0 x < GameConstants.COLS; x++) {
+for(int y = 0 y < GameConstants.ROWS; y++) {
+boolean shade = (x + y) % 2 == 0;
+g2.setColor(shade? new Color(1, 145,1 30): newColor(78,
+242, 110));
+g2.fillRect(x* tile, y* tile, tile, tile);
+}
+}
+}
